4a4afef590e1770d6434e249b7efc373b736fdad
cstaib3 <staib.17@osu.edu>
2023-01-26 16:22:47 -0500

    Working live camera feed in tkinter window

--- videoApplication.py
+++ videoApplication.py
@@ -4,72 +4,62 @@ import cv2
 from PIL import Image
 from PIL import ImageTk
 
-##
-## 'root' objects go first in this order to create the basis of the window before any buttons can be added
-##
+class Video_Capture:
+    def __init__(self, video_source):
+        self.vid = cv2.VideoCapture(video_source)
+        if not self.vid.isOpened():
+            print("Camera Feed Unavailable")
+            exit()
 
-root = ct.CTk() # Creates Tkinter Window
-root.geometry("200x200") # Changes default window size
-root.title('Live Camera View') # Changes window heading title
+        self.width = self.vid.get(cv2.CAP_PROP_FRAME_WIDTH) # Finds the pixel width of the capture
+        self.height = self.vid.get(cv2.CAP_PROP_FRAME_HEIGHT) # Finds the pixel height of the capture
 
-ct.set_appearance_mode("dark") # Sets window appearance to dark mode
-ct.set_default_color_theme("dark-blue") # Sets theme appearance to dark blue
+    def __del__(self):
+        if self.vid.isOpened():
+            self.vid.release()
+        self.window.mainloop()
 
-cap = cv2.VideoCapture(0) # Capture video feed
+    def get_frame(self):
+        if self.vid.isOpened(): # Checks if video feed is accessible
+            ret, frame = self.vid.read() # Takes a snapshot of each frame from the live feed
+            if ret:
+               return (ret, cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)) # Returns the original frame and the RGB Format of the frame
+            else:
+                return (ret, None) 
+        else:
+            return (ret, None)
 
-if not cap.isOpened(): # Checks to make sure camera is accessible
-    print("Camera Feed Unavailable")
-    exit()
+class App:
+    def __init__(self, window, window_title, video_source):
+        self.window = window
+        self.window.title(window_title)
+        self.video_source = video_source
+        self.window.appearance = ct.set_appearance_mode('dark')
 
-cap_width = cap.get(cv2.CAP_PROP_FRAME_WIDTH) # Finds the pixel width of the capture
-cap_height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT) # Finds the pixel height of the capture
+        self.vid = Video_Capture(video_source) # Open Video Source
 
-# class Video_Capture:
-#     def __init__(self, video_source=0)
-#         self.vid = cv2.VideoCapture(video_source)
-#         if not self.vid.isOpened():
-#             print("Camera Feed Unavailable")
-#             exit()
+        self.canvas = ct.CTkCanvas(window, width = self.vid.width, height = self.vid.height)
+        self.canvas.pack()
+        
+        self.delay = 17
+        self.update()
 
-#         cap_width = cap.get(cv2.CAP_PROP_FRAME_WIDTH) # Finds the pixel width of the capture
-#         cap_height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT) # Finds the pixel height of the capture
+        self.window.mainloop()
 
-#     def __del__(self):
-#         if self.vid.isOpened():
-#             self.vid.release()
-#         self.window.mainloop()
+    def update(self):
+        ret, frame = self.vid.get_frame()
 
-#     def get_frame(self):
-#         if self.vid.isOpened():
-#             ret, frame = self.vid.read()
-#             if ret:
-#                return (ret, cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
-#             else:
-#                 return (ret, None)
-#         else:
-#             return (ret, None)
+        if ret:
+            self.photo = ImageTk.PhotoImage(image = Image.fromarray(frame))
+            self.canvas.create_image(0, 0, image = self.photo, anchor = ct.NW)
 
-# class App:
-#     def __init__(self, window, window_title, video_source=0):
-#         self.window = window
-#         self.window.title = 
+        self.window.after(self.delay, self.update)
 
-while True:
-
-    ret, videoInput = cap.read() # Reads the camera input
-
-    if not ret: # Checks to make sure the video is able to be read
-        print("Cannot access video")
-        break
-
-    videoRGB = cv2.cvtColor(videoInput, cv2.COLOR_BGR2RGB) # Change video feed from BGR to RGB Format
-    videoPIL = Image.fromarray(videoInput) # Change video feed from RGB to PIL Format
-    videoTK = ImageTk.PhotoImage(videoPIL) # Change video feed from PIL to Tkinter Format
+App(ct.CTk(), 'Live Camera Feed', 0)
 
 
 
-    root.mainloop()
-    Image = ct.CTkImage(light_image = None, dark_image = videoPIL, size = (200,200))
+
 
     
 
