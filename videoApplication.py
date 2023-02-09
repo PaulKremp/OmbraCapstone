b19bb4abefb38ba69b415e46d0c358e403fd24bb
cstaib3 <staib.17@osu.edu>
2023-02-09 15:58:01 -0500

    Added dropdown menus

--- videoApplication.py
+++ videoApplication.py
@@ -30,7 +30,7 @@ class Video_Capture:
             return (ret, None)
 
 class App:
-    def __init__(self, window, window_title, video_source, backend_choice, model_choice):
+    def __init__(self, window, window_title, video_source):
         self.window = window # Creates CTk Root Window
         self.window.title(window_title) # Adds a title to the top of the window
         self.video_source = video_source # Sets video source variable
@@ -38,23 +38,24 @@ class App:
         
         self.vid = Video_Capture(video_source) # Open Video Source
 
-        self.canvas = ct.CTkCanvas(window, width = self.vid.width+200, height = self.vid.height) # Creates a canvas based on the dimensions of the image
-        self.canvas.pack() 
+        self.canvas = ct.CTkCanvas(window, width = self.vid.width, height = self.vid.height) # Creates a canvas based on the dimensions of the image
+        self.canvas.pack(side=ct.LEFT) 
 
         self.delay = 17 # Sets delay to 17ms (nearly 60 Frames Per Second)
         self.update()
 
-        # self.backend_choice = backend_choice
-        # self.model_choice = model_choice
-        # self.menus()
+        self.menus() # Creates the option menus
 
         self.window.mainloop() # Starts the CTk Window
 
-    # def menus(self):
+    def menus(self):
+        self.backend_choice = ct.StringVar(value='Choose a Backend')
+        self.window.backend_dropdown = ct.CTkOptionMenu(master = self.window, values = ['opencv', 'ssd', 'dlib', 'mtcnn', 'retinaface', 'mediapipe'], variable = self.backend_choice)
+        self.window.backend_dropdown.pack(side=ct.TOP, padx=5, pady=20)
 
-    #     self.window.backend_dropdown = ct.CTkOptionMenu(master = self, values = ['opencv', 'ssd', 'dlib', 'mtcnn', 'retinaface', 'mediapipe'], variable = self.backend_choice)
-    #     self.window.model_dropdown = ct.CTkOptionMenu(master = self, values = ['VGG-Face', 'Facenet', 'Facenet512', 'OpenFace', 'DeepFace', 'DeepID', 'ArcFace', 'Dlib', 'SFace'], variable = self.model_choice)
-
+        self.model_choice = ct.StringVar(value='Choose a Model')
+        self.window.model_dropdown = ct.CTkOptionMenu(master = self.window, values = ['VGG-Face', 'Facenet', 'Facenet512', 'OpenFace', 'DeepFace', 'DeepID', 'ArcFace', 'Dlib', 'SFace'], variable = self.model_choice)
+        self.window.model_dropdown.pack(side=ct.TOP, pady=10)
 
     def update(self):
         ret, frame = self.vid.get_frame() # Snapshots the current frame from the camera feed
@@ -63,16 +64,12 @@ class App:
             self.photo = ImageTk.PhotoImage(image = Image.fromarray(frame)) # Converts this frame from RGB to CTk Format
             self.canvas.create_image(0, 0, image = self.photo, anchor = ct.NW) # Adds the image to the canvas
 
-        self.window.after(self.delay, self.update) # Updates the image every 'self.delay' ms 
+        self.window.after(self.delay, self.update) # Updates the image every 'self.delay' ms
 
 videoSource = 'rtsp://admin:sLUx5%23!!@192.168.40.42:554/cam/realmonitor?channel=1&subtype=00&authbasic=YWRtaW46c0xVeDUlMjMhIQ=='
-App(ct.CTk(), 'Live Camera Feed', videoSource, 'opencv', 'VGG-Face')
+App(ct.CTk(), 'Live Camera Feed', videoSource)
 
 
-
-
-
-    ## Using canvas instead of Frame, create a super().__init__() call using Canvas in App(Canvas)
 
     
 
