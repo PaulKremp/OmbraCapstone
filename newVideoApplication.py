11845e00422258b06241b004197a961a9632095e
cstaib3 <staib.17@osu.edu>
2023-04-06 17:29:32 -0400

    Added functioning done button and default values to options menu

--- newVideoApplication.py
+++ newVideoApplication.py
@@ -13,11 +13,13 @@ from FacialRecognition.utils import FaceDetect
 class Video_Capture:
     def __init__(self, video_source):
         self.vid = cv2.VideoCapture(video_source) # Takes in the video source as a variable
-        recognizerBackend = "VGG-Face"
-        faceDetectorBackend = "opencv"
-        self.embeddings = EmbeddingGen("./db", recognizerBackend).outputEmbeddings(faceDetectorBackend)
-        self.faceDetector = FaceDetect(faceDetectorBackend)
-        self.faceRecognizer = Recognizer(recognizerBackend, self.embeddings, faceDetectorBackend)
+        self.recognizerBackend = "VGG-Face"
+        self.faceDetectorBackend = "opencv"
+
+        self.embeddings = EmbeddingGen("./db", self.recognizerBackend).outputEmbeddings(self.faceDetectorBackend)
+        self.faceDetector = FaceDetect(self.faceDetectorBackend)
+        self.faceRecognizer = Recognizer(self.recognizerBackend, self.embeddings, self.faceDetectorBackend)
+        
         self.Q = queue.Queue()
         if not self.vid.isOpened(): # Checks if the video feed is available
             print("Camera Feed Unavailable")
@@ -44,6 +46,7 @@ class Video_Capture:
             if ret:
                 # Returns the frame with the bounding boxes around the faces and the RGB Format of the frame
                 return (ret, cv2.cvtColor(captureImageWithBoxes, cv2.COLOR_BGR2RGB))
+            
 
 class App():
     def __init__(self, video_source):
@@ -61,6 +64,7 @@ class App():
 
         self.settings_button = ct.CTkButton(self.window, fg_color = "#EA2100", hover_color = "#BA1B01", text_color = "#000000", width = 125, height = 125, text = "Settings", command = self.openSettings).pack(side = ct.TOP, padx = 5, pady = 2)
         self.screenshot_button = ct.CTkButton(self.window, fg_color = "#EA2100", hover_color = "#BA1B01", text_color = "#000000", width = 125, height = 125, text = "Screenshot", command = self.takeScreenshot).pack(side = ct.BOTTOM, padx = 5, pady = 2)
+        
         self.window.mainloop()
 
     def update(self):
@@ -77,21 +81,26 @@ class App():
         print("Screenshot")
 
     def openSettings(self):
-        settingsWindow = ct.CTkToplevel(self.window)
-        settingsWindow.attributes("-topmost", True)
-        settingsWindow.geometry('300x200')
-        settingsWindow.title("Settings")
+        self.settingsWindow = ct.CTkToplevel(self.window)
+        self.settingsWindow.attributes("-topmost", True)
+        self.settingsWindow.geometry('300x200')
+        self.settingsWindow.title("Settings")
+        self.recognizerOption = ct.StringVar(value = self.vid.recognizerBackend)
+        self.backendOption = ct.StringVar(value = self.vid.faceDetectorBackend)
 
-        self.backend_choice = ct.StringVar(value='Choose a Backend') # Sets an initial value for the dropdown menu
-        self.backend_dropdown = ct.CTkOptionMenu(master = settingsWindow, fg_color = "#EA2100", dropdown_fg_color = "#EA2100", dropdown_hover_color = "#BA1B01", text_color = "#000000", dropdown_text_color = "#000000", button_hover_color = "#BA1B01", button_color = "#BA1B01", values = ['opencv', 'ssd', 'dlib', 'mtcnn', 'retinaface', 'mediapipe'], variable = self.backend_choice) # Creates choices for the dropdown menu
+        self.backend_dropdown = ct.CTkOptionMenu(master = self.settingsWindow, fg_color = "#EA2100", dropdown_fg_color = "#EA2100", dropdown_hover_color = "#BA1B01", text_color = "#000000", dropdown_text_color = "#000000", button_hover_color = "#BA1B01", button_color = "#BA1B01", values = ['opencv', 'ssd', 'dlib', 'mtcnn', 'retinaface', 'mediapipe'], variable = self.backendOption) # Creates choices for the dropdown menu
         self.backend_dropdown.pack(side = ct.TOP, padx = 5, pady = 20) # Places the dropdown at a location within the window
 
-        self.model_choice = ct.StringVar(value='Choose a Model') # Sets an initial value for the dropdown menu
-        self.model_dropdown = ct.CTkOptionMenu(master = settingsWindow, fg_color = "#EA2100", dropdown_fg_color = "#EA2100", dropdown_hover_color = "#BA1B01", text_color = "#000000", dropdown_text_color = "#000000", button_hover_color = "#BA1B01", button_color = "#BA1B01", values = ['VGG-Face', 'Facenet', 'Facenet512', 'OpenFace', 'DeepFace', 'DeepID', 'ArcFace', 'Dlib', 'SFace'], variable = self.model_choice) # Creates choices for the dropdown menu
+        self.model_dropdown = ct.CTkOptionMenu(master = self.settingsWindow, fg_color = "#EA2100", dropdown_fg_color = "#EA2100", dropdown_hover_color = "#BA1B01", text_color = "#000000", dropdown_text_color = "#000000", button_hover_color = "#BA1B01", button_color = "#BA1B01", values = ['VGG-Face', 'Facenet', 'Facenet512', 'OpenFace', 'DeepFace', 'DeepID', 'ArcFace', 'Dlib', 'SFace'], variable = self.recognizerOption) # Creates choices for the dropdown menu
         self.model_dropdown.pack(side=ct.TOP, pady=10) # Places the dropdown at a location within the window
 
-        settingsWindow.mainloop()
-         
+        self.done_button = ct.CTkButton(master = self.settingsWindow, fg_color = "#EA2100", hover_color = "#BA1B01", text_color = "#000000", width = 10, height = 10, text = "Done", command = self.refreshSettings).pack(side = ct.BOTTOM, padx = 5, pady = 10)
+        self.settingsWindow.mainloop()
+
+    def refreshSettings(self):
+        self.settingsWindow.destroy()
+
+        
 videoSource = 0
 #videoSource = 'rtsp://admin:sLUx5%23!!@192.168.40.42:554/cam/realmonitor?channel=1&subtype=0'
 App(videoSource)
